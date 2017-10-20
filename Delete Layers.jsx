53a3697acc1538d4fadc00d6c504a40f9aabfac6
rakesh <32685734+rakeshpat@users.noreply.github.com>
2017-10-20 22:23:45 +0100

    added more search options

--- Delete Layers.jsx
+++ Delete Layers.jsx
@@ -1,7 +1,15 @@
 #target photoshop
 
-// dialog box to allow users to input a keyword
-var dlg = new Window("dialog", "Remove layers with names containing...");
+/* dialog box to allow users to select a search option and input a word for use with
+   regex */
+var dlg = new Window("dialog", "Remove layers with names...");
+	var options = dlg.add("group");
+		options.orientation = "row";
+		options.alignChildren = "left";
+		var rbBegins = options.add("radiobutton", undefined, "Beginning with");
+		rbBegins.value = true;
+		var rbContains = options.add("radiobutton", undefined, "Containing");
+		var rbEnds = options.add("radiobutton", undefined, "Ending with");
 	var textInput = dlg.add("edittext", undefined, "");
 		textInput.active = true;
 		textInput.characters = 20;
@@ -12,6 +20,7 @@ dlg.show();
 
 var inputFolder = Folder.selectDialog("Select the folder containing your images");
 var outputFolder = Folder.selectDialog("Select or create a folder for your new images");
+var regExp = new RegExp();
 
 while (inputFolder.toString() == outputFolder.toString()) {
 	alert("The folder you select cannot be the same as the input folder. Choose a different folder.");
@@ -21,16 +30,21 @@ while (inputFolder.toString() == outputFolder.toString()) {
 // set the file list to look only at psd files
 var fileList = inputFolder.getFiles("*.psd");
 
-// regular expression to remove layers matching the regex pattern 
-var regExp = new RegExp(textInput.text, 'i');
-
 if (fileList.length == 0)
 	alert("There are no PSD files in this folder");
 else {
 
-	// opens each psd file from the input folder
+	/* opens each psd file from the input folder and sets up regular expressions depending
+	   on user input of the first dialog box */
 	for (var i = 0; i < fileList.length; i++) {
 		var doc = open(fileList[i]);
+		if (rbBegins.value == true) {
+			regExp = new RegExp('^' + textInput.text, 'i');
+		} else if (rbContains.value == true) {
+			regExp = new RegExp(textInput.text, 'i');
+		} else {
+			regExp = new RegExp(textInput.text + '$', 'i');
+		}
 
 		/* iterates through each layer of the active document and deletes layers with
 		   names that match the regex pattern */
