05b4bd6803bf8c39c5712892ad707ff004a355fa
rakesh <32685734+rakeshpat@users.noreply.github.com>
2017-10-22 15:42:05 +0100

    moved code into functions, changed default search option, added behaviour to selection of cancel buttons and empty inputbox

--- Delete Layers.jsx
+++ Delete Layers.jsx
@@ -1,57 +1,90 @@
 #target photoshop
 
-/* dialog box to allow users to select a search option and input a word for use with
-   regex */
-var dlg = new Window("dialog", "Remove layers with names...");
-	var options = dlg.add("group");
-		options.orientation = "row";
-		var rbBegins = options.add("radiobutton", undefined, "Beginning with");
-		rbBegins.value = true;
-		var rbContains = options.add("radiobutton", undefined, "Containing");
-		var rbEnds = options.add("radiobutton", undefined, "Ending with");
-	var textInput = dlg.add("edittext", undefined, "");
-		textInput.active = true;
-		textInput.characters = 20;
-	var buttons = dlg.add("group");
-		var btnOK = buttons.add("button", undefined, "OK");
-		var btnCancel = buttons.add ("button", undefined, "Cancel");
-dlg.show();
+var regExp = new RegExp();
+var textInput = "";
 
-var inputFolder = Folder.selectDialog("Select the folder containing your images");
-var outputFolder = Folder.selectDialog("Select or create a folder for your new images");
+optionsDialog();
 
-// sets up a regular expression based on users selection of first dialog box
-if (rbBegins.value == true)
-	var regExp = new RegExp('^' + textInput.text, 'i');
-if (rbContains.value == true)
-	var regExp = new RegExp(textInput.text, 'i');
-if (rbEnds.value == true)
-	var regExp = new RegExp(textInput.text + '$', 'i');
+// dialog box to allow users to select a search option and input a word for use with regex
+function optionsDialog() {
+	var dlg = new Window("dialog", "Remove layers with names...");
 
-while (inputFolder.toString() == outputFolder.toString()) {
-	alert("The folder you select cannot be the same as the input folder. Choose a different folder.");
-	var outputFolder = Folder.selectDialog("Select or create a folder for your new images");
+		var options = dlg.add("group");
+			options.orientation = "row";
+			var rbBegins = options.add("radiobutton", undefined, "Beginning with");
+			var rbContains = options.add("radiobutton", undefined, "Containing");
+			var rbEnds = options.add("radiobutton", undefined, "Ending with");
+			rbContains.value = true;
+
+		textInput = dlg.add("edittext", undefined, "");
+			textInput.characters = 20;
+			textInput.active = true;
+
+		var buttons = dlg.add("group");
+
+			var btnOK = buttons.add("button", undefined, "OK");
+				btnOK.onClick = function() {
+					if (textInput.text == "")
+						alert("The text field cannot be left blank");
+					else {
+						dlg.hide();
+
+						// sets up a regular expression based on users selection of first dialog box
+						if (rbBegins.value == true)
+							regExp = new RegExp('^' + textInput.text, 'i');
+						if (rbContains.value == true)
+							regExp = new RegExp(textInput.text, 'i');
+						if (rbEnds.value == true)
+							regExp = new RegExp(textInput.text + '$', 'i');
+
+						main();
+					}
+				}
+
+			var btnCancel = buttons.add ("button", undefined, "Cancel");
+				btnCancel.onClick = function() {
+					dlg.hide();
+					return;
+				}
+	dlg.show();
 }
 
-// set the file list to look only at psd files
-var fileList = inputFolder.getFiles("*.psd");
+function main() {
+	var inputFolder = Folder.selectDialog("Select the folder containing your images");
+	if (inputFolder == null)
+		return;
 
-if (fileList.length == 0)
-	alert("There are no PSD files in this folder");
-else {
-	// opens each psd file from the input folder
-	for (var i = 0; i < fileList.length; i++) {
-		var doc = open(fileList[i]);
+	var outputFolder = Folder.selectDialog("Select or create a folder for your new images");
+	if (outputFolder == null)
+		return;
 
-		/* iterates through each layer of the active document and deletes layers with
-		   names that match the regex pattern */
-		for (var j = 0; j < doc.layers.length; j++) {
-			if (regExp.test(doc.layers[j].name)) {
-				doc.layers[j].remove();
+	while (inputFolder.toString() == outputFolder.toString()) {
+		alert("The folder you select cannot be the same as the input folder. Choose a different folder.");
+		var outputFolder = Folder.selectDialog("Select or create a folder for your new images");
+		if (outputFolder == null)
+			return;
+	}
+
+	// set the file list to look only at psd files
+	var fileList = inputFolder.getFiles("*.psd");
+
+	if (fileList.length == 0)
+		alert("There are no PSD files in this folder");
+	else {
+		// opens each psd file from the input folder
+		for (var i = 0; i < fileList.length; i++) {
+			var doc = open(fileList[i]);
+
+			/* iterates through each layer of the active document and deletes layers with
+			   names that match the regex pattern */
+			for (var j = 0; j < doc.layers.length; j++) {
+				if (regExp.test(doc.layers[j].name)) {
+					doc.layers[j].remove();
+				}
 			}
-		}
 
-		// saves the file in the output folder
-		doc.saveAs(new File(outputFolder + "/" + doc.name), new PhotoshopSaveOptions())
+			// saves the file in the output folder
+			doc.saveAs(new File(outputFolder + "/" + doc.name), new PhotoshopSaveOptions())
+		}
 	}
 }
