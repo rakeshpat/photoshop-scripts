93757097e088f0e693c89e752aa3ad7847c33f9c
rakesh <32685734+rakeshpat@users.noreply.github.com>
2017-11-25 18:55:48 +0000

    new slider for opacity and began writing code for positioning watermark

--- Add Watermark.jsx
+++ Add Watermark.jsx
@@ -29,6 +29,7 @@ var dlg = new Window("dialog", "Options");
 		offsetPnl.orientation = "row";
 		var sOffset = offsetPnl.add("slider", undefined, 10, 0, 100);
 		var ibOffset = offsetPnl.add("edittext", undefined, 10);
+		offsetPnl.add("statictext", undefined, "%");
 		sOffset.onChanging = function() {
 			ibOffset.text = sOffset.value;
 		}
@@ -36,11 +37,17 @@ var dlg = new Window("dialog", "Options");
 			sOffset.value = Number(ibOffset.text);
 		}
 
-	var opacityGrp = dlg.add("group");
-		opacityGrp.orientation = "row";
-		opacityGrp.add("statictext", undefined, "Opacity:");
-		var ibOpacity = opacityGrp.add("edittext", undefined, 10);
-		opacityGrp.add("statictext", undefined, "%");
+	var opacityPnl = dlg.add("panel", undefined, "Opacity");
+		opacityPnl.orientation = "row";
+		var sOpacity = opacityPnl.add("slider", undefined, 10, 0, 100);
+		var ibOpacity = opacityPnl.add("edittext", undefined, 10);
+		opacityPnl.add("statictext", undefined, "%");
+		sOpacity.onChanging = function() {
+			ibOpacity.text = sOpacity.value;
+		}
+		ibOpacity.onChanging = function() {
+			sOpacity.value = Number(ibOpacity.text);
+		}
 
 	var buttons = dlg.add("group");
 		var btnOK = buttons.add("button", undefined, "OK");
@@ -67,8 +74,23 @@ for (var i = 0; i < fileList.length; i++) {
 		app.activeDocument = doc;
 		doc.paste();
 		wmLayer.move(activeDocument, ElementPlacement.PLACEATBEGINNING);
-		doc.artLayers.getByName("watermark").opacity = 10;
+
+		doc.artLayers.getByName("watermark").opacity = sOpacity.value;
+		
+		if (!rbCentre.value && !rbMiddle.value) {
+			if (rbLeft.value) {
+				moveLayer(doc.artLayers.getByName("watermark"), 0, 0);
+			}
+		}
 	}
+}
+
+function moveLayer(layerRef, x, y) {
+	var position = layerRef.bounds;
+	
+	position[0] = x - position[0];
+	position[1] = y - position[1];
+	layerRef.translate(-position[0], -position[1]);
 }
 
 wm.close(SaveOptions.DONOTSAVECHANGES);
